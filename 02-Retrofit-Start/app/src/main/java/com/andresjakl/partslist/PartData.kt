b61de3a8323adfa78fe6dfe64fc3f48957f87bdc
Andreas Jakl <andreas.jakl@live.com>
2019-01-25 20:01:02 +0100

    Add starter project for Retrofit example

---
+++ 02-Retrofit-Start/app/src/main/java/com/andresjakl/partslist/PartData.kt
@@ -0,0 +1,7 @@
+package com.andresjakl.partslist
+
+import com.squareup.moshi.JsonClass
+
+// TODO Add @JsonClass annotation. As parameter, set generateAdapter to true
+
+data class PartData ( var id: Long, var itemName: String)
---
+++ 02-Retrofit-Start/app/src/main/java/com/andresjakl/partslist/WebAccess.kt
@@ -0,0 +1,31 @@
+package com.andresjakl.partslist
+
+import android.util.Log
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+// Singleton pattern in Kotlin: https://kotlinlang.org/docs/reference/object-declarations.html#object-declarations
+object WebAccess {
+    // TODO uncomment following line
+    //val partsApi : PartsApiClient by lazy {
+
+        // TODO Create local variable with Retrofit.Builder()
+
+                // TODO add .baseUrl to the call
+                // If working with a local server on your PC and want to access
+                // it from the emulator, use http://10.0.2.2:3000
+
+                // TODO add Moshi to the builder as converter factory
+                // use: MoshiConverterFactory.create()
+
+                // TODO add the Coroutine call adapter factory
+                // use: CoroutineCallAdapterFactory()
+
+                // TODO use build() to finalize the builder process
+
+
+        // TODO uncomment the following line to create Retrofit client
+        // return@lazy retrofit.create(PartsApiClient::class.java)
+
+}
